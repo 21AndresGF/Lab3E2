d501c6b884fa247124edf374d1d7b95ab8c7baff
Andres Guzman <j.andres.guzman0021@gmail.com>
2020-02-10 15:41:36 -0600

    metodo insercion hoja

--- Lab3E2/Lab3E2/Models/hoja.cs
+++ Lab3E2/Lab3E2/Models/hoja.cs
@@ -5,7 +5,59 @@ using System.Threading.Tasks;
 
 namespace Lab3E2.Models
 {
-    public class hoja
+    public class hoja<Bebidas> where Bebidas :IComparable
     {
+        #region varibles globales
+        //raiz
+        string status { get; set; }
+        //padre
+        int padre { get; set; }
+        //valores de la clase bebidas
+        List<Bebidas> valores;
+        //hijos
+        List<int> hijos;
+        //id
+        int id { get; set; }
+        //rango de nodo
+        int rango;
+
+        #endregion
+        //constructor de la clase hoja
+        public hoja() 
+        {
+            valores = new List<Bebidas>();
+            hijos = new List<int>();
+        }
+
+        public void Insertar(Bebidas dato) 
+        {
+            //es raiz
+            if (status != "raiz")
+            {
+                //no es padre
+                if (hijos.Count == 0)
+                {
+                    //
+                }
+                //si es padre
+                else
+                {
+                    //recorrer hijos
+                    for (int i = 0; i < hijos.Count; i++)
+                    {
+                        if (valores[i].CompareTo(dato))
+                        {
+
+                        }
+                    }
+                }
+            }
+            //no es raiz
+            else
+            {
+
+            }
+        }
+
     }
 }
